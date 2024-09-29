be9434e4565d3c0550077ad376df02e4203c2d2a
yerim <pppp5097@naver.com>
2024-09-29 22:41:55 +0900

    feat: 출결정보 기능 추가

--- src/app/(route)/(afterLogin)/attendance/page.tsx
+++ src/app/(route)/(afterLogin)/attendance/page.tsx
@@ -1,24 +1,49 @@
 "use client";
 
-import React from "react";
-import { Calendar } from "antd";
+import React, { useState } from "react";
+import { Calendar, Segmented } from "antd";
 import dayjs from "dayjs";
 
 const AttendancePage = () => {
+  const [selectedDate, setSelectedDate] = useState(dayjs("2024-09-01"));
+
+  const onMonthChange = (value: string) => {
+    if (value === "9월") {
+      setSelectedDate(dayjs("2024-09-01"));
+    } else if (value === "10월") {
+      setSelectedDate(dayjs("2024-10-01"));
+    }
+  };
+
   return (
-    <div className="w-[400px]">
-      <Calendar
-        fullscreen={false}
-        headerRender={(value) => {
-          return <h3 className="mb-3">{dayjs(value.value).format("M월")}</h3>;
-        }}
-        cellRender={(current, info) => {
-          if (dayjs(current).isSame(dayjs("2024-09-02")))
-            return <div>{"출석"}</div>;
-          else return null;
-        }}
-        validRange={[dayjs("2024-09-01"), dayjs("2024-10-31")]}
+    <div className="w-[400px] flex flex-col gap-5">
+      <Segmented
+        className="w-fit"
+        value={`${selectedDate.month() + 1}월`}
+        options={["9월", "10월"]}
+        onChange={(value) => onMonthChange(value as string)}
       />
+      <div className="">
+        <Calendar
+          fullscreen={false}
+          value={selectedDate}
+          headerRender={(value) => {
+            return <h3 className="mb-3">{dayjs(value.value).format("M월")}</h3>;
+          }}
+          cellRender={(current, info) => {
+            console.log("🚀 ~ AttendancePage ~ current:", current);
+            if (dayjs(current).isSame(dayjs("2024-09-02")))
+              return (
+                <div className="text-[10px] leading-4 text-red-700">
+                  {"결석"}
+                </div>
+              );
+            else return <div className="text-[10px] leading-4">{"출석"}</div>;
+          }}
+          onChange={(v) => setSelectedDate(v)}
+          onPanelChange={(value) => setSelectedDate(value)}
+        />
+      </div>
     </div>
   );
 };
