9e4788032a7e8861f25e0a0d4c275fcbf8696f13
yerim <pppp5097@naver.com>
2024-10-04 00:03:57 +0900

    feat: 출결정보 데이터 연동

--- src/app/(route)/(afterLogin)/attendance/page.tsx
+++ src/app/(route)/(afterLogin)/attendance/page.tsx
@@ -1,47 +1,91 @@
 "use client";
 
 import React, { useState } from "react";
-import { Calendar, Segmented } from "antd";
+import { Calendar, Segmented, Tag } from "antd";
 import dayjs from "dayjs";
+import { CheckCircleOutlined, ScheduleTwoTone } from "@ant-design/icons";
+import useSWR from "swr";
 
 const AttendancePage = () => {
-  const [selectedDate, setSelectedDate] = useState(dayjs("2024-09-01"));
+  const [month, setMonth] = useState(0);
 
-  const onMonthChange = (value: string) => {
-    if (value === "9월") {
-      setSelectedDate(dayjs("2024-09-01"));
-    } else if (value === "10월") {
-      setSelectedDate(dayjs("2024-10-01"));
-    }
-  };
+  const { data, error, isLoading } = useSWR("/api/getAttendanceData", () =>
+    fetch(`/api/getAttendanceData`).then(async (res) => {
+      const { data } = await res.json();
+      return [data[0].values, data[1].values];
+    })
+  );
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
+
+  const titles = [data[0][0][0], data[1][0][0]];
 
   return (
-    <div className="w-[400px] flex flex-col gap-5">
+    <div className="w-[400px] max-w-[90vw] flex flex-col gap-5">
+      <h5 className="flex items-center gap-2">
+        <ScheduleTwoTone />
+        출결정보
+      </h5>
       <Segmented
         className="w-fit"
-        value={`${selectedDate.month() + 1}월`}
-        options={["9월", "10월"]}
-        onChange={(value) => onMonthChange(value as string)}
+        value={month}
+        options={[
+          { label: titles[0], value: 0 },
+          { label: titles[1], value: 1 },
+        ]}
+        onChange={(v) => setMonth(v)}
       />
-      <div className="">
-        <Calendar
-          fullscreen={false}
-          value={selectedDate}
-          headerRender={(value) => {
-            return <h3 className="mb-3">{dayjs(value.value).format("M월")}</h3>;
-          }}
-          cellRender={(current, info) => {
-            if (dayjs(current).isSame(dayjs("2024-09-02")))
-              return (
-                <div className="text-[10px] leading-4 text-red-700">
-                  {"결석"}
-                </div>
-              );
-            else return <div className="text-[10px] leading-4">{"출석"}</div>;
-          }}
-          onChange={(v) => setSelectedDate(v)}
-          onPanelChange={(value) => setSelectedDate(value)}
-        />
+      <div className="text-center grid grid-cols-7 gap-x-1 row-span-5">
+        {data[month].slice(1).map((row: string[], rowIndex: number) => {
+          const arr = row;
+          while (arr.length < 8) {
+            arr.push("");
+          }
+          return arr.map((cell, cellIndex) => {
+            if (cellIndex === 0) return null;
+            const isText = rowIndex > 0 && cell && !Number(cell);
+            const isSunday = cellIndex === 1;
+            const isSaturday = cellIndex === 7;
+            return (
+              <div
+                className={`h-[30px] flex flex-col gap-1 ${
+                  isText
+                    ? ""
+                    : isSunday
+                    ? "text-red-500"
+                    : isSaturday
+                    ? "text-blue-500"
+                    : ""
+                }`}
+                key={cellIndex}
+              >
+                {isText ? (
+                  cell.split("\n").map((line, index) => (
+                    <React.Fragment key={index}>
+                      <Tag
+                        color={
+                          line === "출석"
+                            ? "processing"
+                            : line === "결석"
+                            ? "error"
+                            : line === "지각"
+                            ? "warning"
+                            : ""
+                        }
+                        className="!m-0"
+                      >
+                        <div className="truncate">{line}</div>
+                      </Tag>
+                    </React.Fragment>
+                  ))
+                ) : (
+                  <>{cell}</>
+                )}
+              </div>
+            );
+          });
+        })}
       </div>
     </div>
   );
---
+++ src/app/api/getAttendanceData/route.ts
@@ -0,0 +1,31 @@
+import { google } from "googleapis";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  try {
+    const auth = new google.auth.GoogleAuth({
+      credentials: {
+        client_email: process.env.GOOGLE_CLIENT_EMAIL,
+        private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+      },
+      scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
+    });
+
+    const sheets = google.sheets({ version: "v4", auth: auth });
+
+    const spreadsheetId = "1xwR133yh4OEcx3zfhdRUkzd6Tfy95aX2yIyAhoEs2PE";
+    const ranges = [`출석(1월)!B2:I23`, `출석(2월)!B2:I23`];
+
+    const response = await sheets.spreadsheets.values.batchGet({
+      spreadsheetId,
+      ranges,
+    });
+
+    // Extract values and merged cell information
+    const values = response.data.valueRanges;
+
+    return NextResponse.json({ data: values });
+  } catch (error) {
+    throw new Error("Error fetching data from Google Spreadsheet");
+  }
+}
