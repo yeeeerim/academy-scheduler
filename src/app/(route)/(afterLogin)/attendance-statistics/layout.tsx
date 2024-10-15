a3881e85567fb22fda66fc21398b9c65f76e41af
yejin <yeajin357@naver.com>
2024-10-15 20:08:37 +0900

    feat: 출결정보 통계 layout.tsx 추가

--- src/app/(route)/(afterLogin)/attendance-statistics/layout.tsx
+++ src/app/(route)/(afterLogin)/attendance-statistics/layout.tsx
@@ -1,12 +1,12 @@
 export const metadata = {
-  title: "출결정보 캘린더 ∙ 윈터스쿨",
+  title: "출결정보 통계 ∙ 윈터스쿨",
 };
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex flex-col gap-3">
       <h5 className="flex items-center gap-2 font-bold pl-1 sm:pl-3 sm:text-[16px]">
-        출결정보 캘린더
+        출결정보 통계
       </h5>
       <div className="bg-white rounded-md p-6 sm:!rounded-none sm:p-3">
         {children}
