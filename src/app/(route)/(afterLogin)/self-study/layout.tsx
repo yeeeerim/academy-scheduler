3807ff45da4bcec6c8ebdd8e25ad3ce2ba74050c
yerim <pppp5097@naver.com>
2024-12-30 15:31:50 +0900

    빌드 에러 수정

--- src/app/(route)/(afterLogin)/self-study/layout.tsx
+++ src/app/(route)/(afterLogin)/self-study/layout.tsx
@@ -1,3 +1,5 @@
+import { Suspense } from "react";
+
 export const metadata = {
   title: "자습교재 현황 ∙ 윈터스쿨",
 };
@@ -5,8 +7,18 @@ export const metadata = {
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex flex-col gap-3">
-      <h5 className="flex items-center gap-2 font-bold pl-1 sm:pl-3 sm:text-[16px]">자습교재 현황</h5>
-      <div>{children}</div>
+      <h5 className="flex items-center gap-2 font-bold pl-1 sm:pl-3 sm:text-[16px]">
+        자습교재 현황
+      </h5>
+      <div>
+        <Suspense
+          fallback={
+            <div className="w-[184px] h-[32px] bg-[#efefef] rounded-[6px] animate-fade sm:mx-3" />
+          }
+        >
+          {children}
+        </Suspense>
+      </div>
     </div>
   );
 };
