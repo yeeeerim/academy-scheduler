6c58f92f7c6a36296652cde7d2518bcd47ecb730
yerim <pppp5097@naver.com>
2025-01-31 10:25:13 +0900

    fix: row-key index 오류 수정

--- src/app/(route)/(afterLogin)/(onlyAdmin)/management/page.tsx
+++ src/app/(route)/(afterLogin)/(onlyAdmin)/management/page.tsx
@@ -1,14 +1,14 @@
-"use client";
+'use client';
 
-import React from "react";
-import { Button, Table, Tag, Tooltip } from "antd";
-import type { TableProps } from "antd";
-import useSWR from "swr";
+import React from 'react';
+import { Button, Table, Tag, Tooltip } from 'antd';
+import type { TableProps } from 'antd';
+import useSWR from 'swr';
 
-import userList from "/tmp/data.json";
-import { ExclamationCircleTwoTone } from "@ant-design/icons";
-import { setCookie } from "cookies-next";
-import toast from "react-hot-toast";
+import userList from '/tmp/data.json';
+import { ExclamationCircleTwoTone } from '@ant-design/icons';
+import { setCookie } from 'cookies-next';
+import toast from 'react-hot-toast';
 
 interface DataType {
   key: string;
@@ -20,15 +20,15 @@ interface DataType {
 }
 
 const page = () => {
-  const { data: accountInfo, isLoading } = useSWR("/api/getAccountInfo", () => {
-    return fetch("/api/getAccountInfo").then((res) => res.json());
+  const { data: accountInfo, isLoading } = useSWR('/api/getAccountInfo', () => {
+    return fetch('/api/getAccountInfo').then((res) => res.json());
   });
 
-  const columns: TableProps<DataType>["columns"] = [
+  const columns: TableProps<DataType>['columns'] = [
     {
-      title: "이름",
-      dataIndex: "name",
-      key: "name",
+      title: '이름',
+      dataIndex: 'name',
+      key: 'name',
       sorter: (a, b) => a.name.localeCompare(b.name),
       showSorterTooltip: false,
       render: (_, { name, isFetched }) => (
@@ -45,40 +45,40 @@ const page = () => {
       ),
     },
     {
-      title: "아이디",
-      dataIndex: "id",
-      key: "id",
+      title: '아이디',
+      dataIndex: 'id',
+      key: 'id',
       // sorter: (a, b) => a.id.localeCompare(b.id),
       // showSorterTooltip: false,
     },
     {
-      title: "비밀번호",
-      key: "password",
+      title: '비밀번호',
+      key: 'password',
       filters: [
-        { text: "설정완료", value: true },
-        { text: "미설정", value: false },
+        { text: '설정완료', value: true },
+        { text: '미설정', value: false },
       ],
       filterSearch: true,
       onFilter: (value, record) => record.password === value,
-      dataIndex: "password",
+      dataIndex: 'password',
       render: (_, { password }) => (!!password ? <Tag color="processing">설정완료</Tag> : <Tag color="error">미설정</Tag>),
     },
     {
-      title: "시트",
-      dataIndex: "sheetId",
-      key: "sheetId",
+      title: '시트',
+      dataIndex: 'sheetId',
+      key: 'sheetId',
       render: (text) => {
-        if (!text) return "-";
+        if (!text) return '-';
         return (
           <a href={`https://docs.google.com/spreadsheets/d/${text}`} target="_blank">
-            {"열기"}
+            {'열기'}
           </a>
         );
       },
     },
     {
-      title: "",
-      key: "action",
+      title: '',
+      key: 'action',
       render: (_, record) => (
         <Button
           className="!text-[12px]"
@@ -88,12 +88,12 @@ const page = () => {
             const now = new Date();
             now.setTime(now.getTime() + 30 * 60 * 1000); // 30분
 
-            setCookie("authToken", record.sheetId, {
+            setCookie('authToken', record.sheetId, {
               expires: now,
             });
 
-            localStorage.setItem("u_name", record.name);
-            window.location.href = "/";
+            localStorage.setItem('u_name', record.name);
+            window.location.href = '/';
 
             toast.success(`${record.name} 로그인 성공`);
           }}
@@ -110,7 +110,7 @@ const page = () => {
           return user.name === item.name;
         });
         return {
-          key: item.id,
+          key: `${item.id}-${item.name}`,
           name: item.name,
           id: item.id,
           password: item.password,
@@ -122,20 +122,20 @@ const page = () => {
 
   return (
     <div className="flex flex-col gap-3">
-      <Tooltip title={data.filter((d) => !d.isFetched).length === 0 ? "데이터가 모두 업데이트 되었습니다." : ""}>
+      <Tooltip title={data.filter((d) => !d.isFetched).length === 0 ? '데이터가 모두 업데이트 되었습니다.' : ''}>
         <Button
           className="w-fit self-end"
           disabled={data.filter((d) => !d.isFetched).length === 0}
           onClick={async () => {
-            if (window.confirm("데이터를 업데이트 하시겠습니까?\n(평균 소요 시간 : 75s)")) {
-              const res = await fetch("https://api.vercel.com/v1/integrations/deploy/prj_jTZH866kxIK7Fgijib9QBneGVowe/XxxUHaGvQE", {
-                method: "POST",
+            if (window.confirm('데이터를 업데이트 하시겠습니까?\n(평균 소요 시간 : 75s)')) {
+              const res = await fetch('https://api.vercel.com/v1/integrations/deploy/prj_jTZH866kxIK7Fgijib9QBneGVowe/XxxUHaGvQE', {
+                method: 'POST',
               });
 
               if (res.ok) {
-                alert("배포가 시작되었습니다.");
+                alert('배포가 시작되었습니다.');
               } else {
-                alert("배포에 실패했습니다.");
+                alert('배포에 실패했습니다.');
               }
             }
           }}
@@ -143,7 +143,7 @@ const page = () => {
           데이터 업데이트
         </Button>
       </Tooltip>
-      <Table<DataType> loading={isLoading} size="small" columns={columns.map((item) => ({ ...item, align: "center" }))} dataSource={data} pagination={false} />
+      <Table<DataType> loading={isLoading} size="small" columns={columns.map((item) => ({ ...item, align: 'center' }))} dataSource={data} pagination={false} />
     </div>
   );
 };
